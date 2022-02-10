df054759814d22de26960c1dc2352684aace7da2
angularsantanderi <angularsantanderi@gmail.com>
2022-02-10 10:26:56 +0100

    validators

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CrearFacturaComponent } from './facturas/crear-factura/crear-factura.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrearFacturaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
